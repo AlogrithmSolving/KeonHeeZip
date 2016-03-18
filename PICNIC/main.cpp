a390665a219f9e767665a58209ab96712557c869
KeonHee <kimgh6554@hanmail.net>
2016-03-19 01:03:34 +0900

    problem solving! ( 초기화 문제 해결)

--- PICNIC/main.cpp
+++ PICNIC/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-
+#include <cassert>
+#include <cstring>
 using namespace std;
 
 /**
@@ -7,44 +8,16 @@ using namespace std;
  *  6.3 문제 : 소풍 (ID : PICNIC, 난이도 : 하 )
  */
 
+bool isFriend[10][10]; //두 학생이 친구이면 true
+int numStd; // 학생의 수
+int numFriend; // 친구 쌍의 수
 
-int countCouple(bool isFriend[][10], bool hasFriend[10], int std);
-int main() {
-
-
-    int numberOfCount = 0; //테스트 케이스 수
-
-
-    bool isFriend[10][10] = {false}; //두 학생이 친구이면 true
-    bool hasFriend[10] = {false}; // 짝이 이루어졌으면 true
-
-    int std = 0 ; // 학생의 수
-    int numFriend = 0 ; // 친구 쌍의 수
-    int f1, f2;
-
-
-
-    cin >> numberOfCount; //테스트 케이스 수 입력
-    for( int i = 0 ; i < numberOfCount ; i++){
-
-        cin >> std >> numFriend; // 학생수, 친구쌍의 수 입력
-        for ( int j = 0 ; j < numFriend ; j++){
-            cin >> f1 >> f2;
-            isFriend[f1][f2]=isFriend[f2][f1] = true;
-        }
-
-        cout << countCouple(isFriend,hasFriend,std) << endl;
-
-    }
-    return 0;
-}
-
-int countCouple(bool isFriend[][10],bool hasFriend[10], int std){
+int countCouple(bool hasFriend[10]){
 
 
     //남은 학생중 가장 번호가 빠른 학생을 찾는다
     int firstStd = -1;
-    for(int i = 0 ; i < std ; i++){
+    for(int i = 0 ; i < numStd ; i++){
         if(!hasFriend[i]){
             firstStd = i;
             break;
@@ -57,14 +30,43 @@ int countCouple(bool isFriend[][10],bool hasFriend[10], int std){
     }
     int count = 0 ;
 
-
     // 번호가 가장빠른 학생의 짝을 찾는다
-    for(int partner = firstStd+1 ; partner < std ; partner++){
-        if (!hasFriend[partner] && isFriend[firstStd][partner] == true){ // 짝이없고, 친구인 학생과 짝을 맺어준다
+    for(int partner = firstStd+1 ; partner < numStd ; partner++){
+        if (!hasFriend[partner] && isFriend[firstStd][partner]){ // 짝이없고, 친구인 학생과 짝을 맺어준다
             hasFriend[firstStd] = hasFriend[partner] = true;
-            count += countCouple(isFriend,hasFriend, std);
+            count += countCouple(hasFriend);
             hasFriend[firstStd] = hasFriend[partner] = false;
         }
     }
-    return count ;
+    return count;
 }
+
+int main() {
+
+    int numCase; //테스트 케이스 수
+
+    cin >> numCase; //테스트 케이스 수 입력
+
+    for( int i = 0 ; i < numCase ; i++){
+
+        cin >> numStd >> numFriend; // 학생수, 친구쌍의 수 입력
+        assert(numStd <= 10);
+        memset(isFriend, 0, sizeof(isFriend));
+
+        for ( int j = 0 ; j < numFriend ; j++){
+            int f1,f2;
+            cin >> f1 >> f2;
+            assert(0 <= f1 && f1 < numStd && 0 <= f2 && f2 < numStd);
+            assert(!isFriend[f1][f2]);
+            isFriend[f1][f2] = isFriend[f2][f1] = true;
+        }
+
+        bool hasFriend[10]; // 짝이 이루어졌으면 true
+        memset(hasFriend, 0, sizeof(hasFriend));
+        cout << countCouple(hasFriend) << endl;
+
+    }
+    return 0;
+}
+
+
